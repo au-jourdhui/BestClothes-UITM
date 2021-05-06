e53304b76223298307de08807ea9bd606aa8b4b0
Patryk Bartman <patryk.bartman.22@gmail.com>
2021-05-06 21:37:59 +0300

    Relations added

--- Scripts/1.sql
+++ Scripts/1.sql
@@ -9,6 +9,9 @@ drop table if exists dbo.[Messages];
 drop table if exists dbo.Orders;
 drop table if exists dbo.Carts;
 drop table if exists dbo.Users;
+drop table if exists dbo.ProductPictures;
+drop table if exists dbo.TaggedProducts;
+drop table if exists dbo.OrderProducts;
 drop table if exists dbo.Tags;
 drop table if exists dbo.Products;
 drop table if exists dbo.Categories;
@@ -73,6 +76,40 @@ Id int not null primary key identity,
 IsDeleted bit not null default (0),
 UserId int not null foreign key references dbo.Users(Id),
 [DateTime] datetime not null default (getdate())
+);
+go
+
+create table dbo.OrderProducts
+(
+Id int not null primary key identity,
+IsDeleted bit not null default (0),
+ProductId int not null foreign key references dbo.Products(Id),
+OrderId int not null foreign key references dbo.Orders(Id),
+Quantity int not null default(1)
+
+unique (ProductId, OrderId)
+);
+go
+
+create table dbo.TaggedProducts
+(
+Id int not null primary key identity,
+IsDeleted bit not null default (0),
+ProductId int not null foreign key references dbo.Products(Id),
+TagId int not null foreign key references dbo.Tags(Id),
+
+unique (ProductId, TagId)
+);
+go
+
+create table dbo.ProductPictures
+(
+Id int not null primary key identity,
+IsDeleted bit not null default (0),
+ProductId int not null foreign key references dbo.Products(Id),
+[Image] varchar(1024) not null
+
+unique ([Image], ProductId)
 );
 go
 
